38d88b214d3fe99eb5827e7f478a9de271a30c77
Dominique Boeckli <dboeckli@gmail.com>
2024-12-14 18:29:13 +0100

    refactor project

---
+++ src/main/java/ch/springframeworkguru/spring6webapp/config/LogbookConfig.java
@@ -0,0 +1,17 @@
+package ch.springframeworkguru.spring6webapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.zalando.logbook.Sink;
+import org.zalando.logbook.json.JsonHttpLogFormatter;
+import org.zalando.logbook.logstash.LogstashLogbackSink;
+
+@Configuration
+public class LogbookConfig {
+
+    @Bean
+    public Sink LogbookLogStash(){
+        return new LogstashLogbackSink(new JsonHttpLogFormatter());
+    }
+   
+}
