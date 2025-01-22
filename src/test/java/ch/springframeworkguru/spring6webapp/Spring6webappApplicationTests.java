d583dbb6364722d47a8395601346a1ecc11e3939
adm-boeckli <dboeckli@gmail.com>
2025-01-22 11:59:52 +0100

    refactor project and build

--- src/test/java/ch/springframeworkguru/spring6webapp/Spring6webappApplicationTests.java
+++ src/test/java/ch/springframeworkguru/spring6webapp/Spring6webappApplicationTests.java
@@ -1,13 +1,16 @@
 package ch.springframeworkguru.spring6webapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+@Slf4j
 class Spring6webappApplicationTests {
 
 	@Test
 	void contextLoads() {
+        log.info("Testing Spring 6 Application...");
 	}
 
 }
