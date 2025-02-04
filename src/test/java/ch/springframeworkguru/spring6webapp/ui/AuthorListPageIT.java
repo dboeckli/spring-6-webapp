04543ecb20cbadf711875e33d9cbad3a7c78c93b
adm-boeckli <dboeckli@gmail.com>
2025-02-04 16:58:00 +0100

    add selenium tests

---
+++ src/test/java/ch/springframeworkguru/spring6webapp/ui/AuthorListPageIT.java
@@ -0,0 +1,58 @@
+package ch.springframeworkguru.spring6webapp.ui;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+
+import java.time.Duration;
+import java.util.Objects;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@Slf4j
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+class AuthorListPageIT {
+
+    @LocalServerPort
+    private int port;
+
+    private WebDriver webDriver;
+
+    @BeforeEach
+    public void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");  // Run in headless mode
+        webDriver = new ChromeDriver(options);
+    }
+
+    @AfterEach
+    public void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+    }
+
+    @Test
+    @Order(0)
+    void testAuthorListPageLoads() {
+        webDriver.get("http://localhost:" + port + "/authors");
+        waitForPageLoad();
+        assertEquals("Author List", webDriver.getTitle());
+    }
+
+    private void waitForPageLoad() {
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(30));
+        wait.until((ExpectedCondition<Boolean>) wd ->
+            Objects.equals(((JavascriptExecutor) wd).executeScript("return document.readyState"), "complete"));
+    }
+}
