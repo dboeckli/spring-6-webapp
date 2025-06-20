bf3dcdea4b7054b255d37cf54d5ebf173c84c6cf
Dominique Boeckli <dboeckli@gmail.com>
2025-06-20 11:46:54 +0200

    improve test (#31)
    * improve test
    
    * upgrade spring boot to 3.5.3

--- src/test/java/ch/springframeworkguru/spring6webapp/Spring6webappApplicationTests.java
+++ src/test/java/ch/springframeworkguru/spring6webapp/Spring6webappApplicationTests.java
@@ -7,13 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
 @Slf4j
 class Spring6webappApplicationTests {
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     @Autowired
     AuthorRepository authorRepository;
@@ -26,7 +29,8 @@ class Spring6webappApplicationTests {
 
 	@Test
 	void contextLoads() {
-        log.info("Testing Spring 6 Application...");
+        assertNotNull(applicationContext, "Application context should not be null");
+        log.info("Testing Spring 6 Application {}", applicationContext.getApplicationName());
         assertAll("Repository counts",
             () -> assertEquals(2, authorRepository.count(), "Author count should be 2"),
             () -> assertEquals(2, bookRepository.count(), "Book count should be 2"),
